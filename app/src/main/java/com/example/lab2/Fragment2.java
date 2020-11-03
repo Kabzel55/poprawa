bee3af5fd8ca3e1e82dcfb649840e46c6d0b8355
Kabzel55 <patryk.koryga@pollub.edu.pl>
2020-11-03 14:09:43 +0100

    Initial comit

---
+++ app/src/main/java/com/example/lab2/Fragment2.java
@@ -0,0 +1,56 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.Observer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Fragment2 extends Fragment {
+
+    private FragsData fragsData;
+    private Observer<Integer> numberObserver;
+
+    private TextView text;
+    private Button button;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        text = (TextView) view.findViewById(R.id.current);
+        button = (Button) view.findViewById(R.id.button_plus);
+
+        fragsData = new ViewModelProvider(requireActivity()).get(FragsData.class);
+
+        numberObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer newInteger) {
+
+                text.setText(newInteger.toString());
+            }
+        };
+
+
+        fragsData.counter.observe(getViewLifecycleOwner(), numberObserver);
+
+        button.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View view) {
+
+                                          Integer i = fragsData.counter.getValue();
+                                          fragsData.counter.setValue(++i);
+                                      }
+                                  }
+        );
+
+        return view;
+    }
+}
